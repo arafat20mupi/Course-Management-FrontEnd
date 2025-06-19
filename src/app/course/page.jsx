0154ef5df20aca5164023e9746569d18300a9ab4
unknown <arafat.innotimes.net@gmail.com>
2025-06-20 00:23:44 +0600

    Setup Done

---
+++ src/app/course/page.jsx
@@ -0,0 +1,11 @@
+
+const page = () => {
+    return (
+        <div>
+            My Course Management Page
+            <p>Welcome to the course management system!</p>
+        </div>
+    );
+};
+
+export default page;
